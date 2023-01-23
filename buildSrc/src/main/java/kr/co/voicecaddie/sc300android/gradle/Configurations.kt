e12ca03ef9559e032fd86d3c89f21570f3483c00
janghee__ <sj90947@gmail.com>
2023-01-24 01:30:01 +0900

    - v1.0.1

--- buildSrc/src/main/java/kr/co/voicecaddie/sc300android/gradle/Configurations.kt
+++ buildSrc/src/main/java/kr/co/voicecaddie/sc300android/gradle/Configurations.kt
@@ -11,7 +11,7 @@ object Configurations {
     const val versionCode = 1
     const val majorVersion = 1
     const val minorVersion = 0
-    const val patchVersion = 0
+    const val patchVersion = 1
     const val developVersion = 0
 
 
